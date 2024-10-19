16dde83479affe9bfbe3fa6a93c14c6c96b41653
fourcolors <sterlingcobb@gmail.com>
2024-10-19 13:23:16 -0700

    add dynamic and story integration

--- app/lib/providers.tsx
+++ app/lib/providers.tsx
@@ -1,38 +1,53 @@
-'use client';
+"use client";
 
 import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
 import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
 import { WagmiProvider } from "wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { DynamicWagmiConnector } from "@dynamic-labs/wagmi-connector";
+import { ZeroDevSmartWalletConnectors } from "@dynamic-labs/ethereum-aa";
 import { config } from "@/lib/wagmi";
 
-
-export default function Providers({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-
+export default function Providers({ children }: { children: React.ReactNode }) {
   const queryClient = new QueryClient();
+
+  const evmNetworks = [
+    {
+      blockExplorerUrls: ["https://testnet.storyscan.xyz/"],
+      chainId: 1513,
+      chainName: "Story Protocol",
+      iconUrls: ["/images/story_icon.svg"],
+      name: "Story",
+      nativeCurrency: {
+        decimals: 18,
+        name: "IP",
+        symbol: "IP",
+      },
+      networkId: 1513,
+      rpcUrls: ["https://testnet.storyrpc.io/"],
+      vanityName: "Story",
+    },
+  ];
 
   return (
     <DynamicContextProvider
       theme="auto"
       settings={{
-        environmentId: "2762a57b-faa4-41ce-9f16-abff9300e2c9",
-           walletConnectors: [EthereumWalletConnectors],
+        environmentId: "d5116454-ef88-4e1b-b193-2b06f24c86b6",
+        walletConnectors: [
+          EthereumWalletConnectors,
+          ZeroDevSmartWalletConnectors,
+        ],
+        overrides: {
+          evmNetworks,
+        },
       }}
     >
-      
       <WagmiProvider config={config}>
         <QueryClientProvider client={queryClient}>
-          <DynamicWagmiConnector>
-            {children}
-          </DynamicWagmiConnector>
+          <DynamicWagmiConnector>{children}</DynamicWagmiConnector>
         </QueryClientProvider>
       </WagmiProvider>
-      
     </DynamicContextProvider>
   );
 }
